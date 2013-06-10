6920315497676bc3ebaf28634af19a2deb554489
Olivier Prouvost <olivier.prouvost@opcoach.com>
2013-06-10 18:20:46 +0200

    Add the EPL headers in file sources, and an about.html

--- com.opcoach.e4.contextExplorer/src/com/opcoach/e4/contextExplorer/parts/ContextExplorerPart.java
+++ com.opcoach.e4.contextExplorer/src/com/opcoach/e4/contextExplorer/parts/ContextExplorerPart.java
@@ -1,3 +1,14 @@
+
+/*******************************************************************************
+ * Copyright (c) 2013 OPCoach.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     OPCoach - initial API and implementation
+ *******************************************************************************/
 package com.opcoach.e4.contextExplorer.parts;
 
 import java.util.Map;
