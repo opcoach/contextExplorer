21a6dcc5a1a6ad6a3c54caf4e899aeb8a54f558b
Olivier Prouvost <olivier.prouvost@opcoach.com>
2013-07-15 17:34:15 +0200

    Etape temporaire pour le search

---
+++ com.opcoach.e4.contextExplorer/src/com/opcoach/e4/contextExplorer/filterTrees/package-info.java
@@ -0,0 +1,10 @@
+/**
+ *  This package is a copy of the FilteredTree of org.eclipse.ui.dialogs got from org.eclipse.ui.workbench plugin.
+ *  And also of String Matcher from package org.eclipse.ui.internal.misc;
+ *  It has been copied to avoid a strong dependency to E3, while the context Explorer is a pure E4 component.
+ *  It will be rebound to the correct classes when they will be available.
+
+ * @author olivier
+ *
+ */
+package com.opcoach.e4.contextExplorer.filterTrees;
